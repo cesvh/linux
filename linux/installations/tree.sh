82ce2616db5aebfae2b59a27c8b638f2af16c76e
cesvh <cesar.vega.hdez@gmail.com>
2021-12-17 12:35:54 -0600

    start in linux mint 21.3

---
+++ linux/installations/tree.sh
@@ -0,0 +1,48 @@
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ tree
+No se ha encontrado la orden «tree», pero se puede instalar con:
+sudo apt install tree
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ sudo apt install tree
+[sudo] contraseña para cvh:     
+Leyendo lista de paquetes... Hecho
+Creando árbol de dependencias... Hecho
+Leyendo la información de estado... Hecho
+Se instalarán los siguientes paquetes NUEVOS:
+  tree
+0 actualizados, 1 nuevos se instalarán, 0 para eliminar y 0 no actualizados.
+Se necesita descargar 47.9 kB de archivos.
+Se utilizarán 116 kB de espacio de disco adicional después de esta operación.
+Des:1 http://archive.ubuntu.com/ubuntu jammy/universe amd64 tree amd64 2.0.2-1 [47.9 kB]
+Descargados 47.9 kB en 1s (94.8 kB/s)
+Seleccionando el paquete tree previamente no seleccionado.
+(Leyendo la base de datos ... 566640 ficheros o directorios instalados actualmen
+te.)
+Preparando para desempaquetar .../tree_2.0.2-1_amd64.deb ...
+Desempaquetando tree (2.0.2-1) ...
+Configurando tree (2.0.2-1) ...
+Procesando disparadores para man-db (2.10.2-1) ...
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ tree
+.
+├── linux
+│   ├── configurations
+│   │   └── nano-to-vim.sh
+│   ├── installations
+│   │   ├── clone-github-repository.sh
+│   │   ├── git.sh
+│   │   ├── nvm.sh
+│   │   └── screenfetch.sh
+│   ├── LICENSE
+│   └── man
+│       └── man.sh
+├── README.md
+└── windows
+    └── vscodium.log
+
+5 directories, 9 files
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
