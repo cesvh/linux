87797e447c6c8c957af2e3027a26f45890d2df2c
cesvh <cesar.vega.hdez@gmail.com>
2021-12-18 12:34:54 -0600

    set datetime

---
+++ linux/configurations/set-datetime.sh
@@ -0,0 +1,77 @@
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ sudo su
+[sudo] contraseña para cvh:     
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# date
+dom 14 jul 2024 00:09:07 CST
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# timedatectl
+               Local time: dom 2024-07-14 00:09:15 CST
+           Universal time: dom 2024-07-14 06:09:15 UTC
+                 RTC time: sáb 2021-12-18 18:33:59
+                Time zone: America/Mexico_City (CST, -0600)
+System clock synchronized: yes
+              NTP service: active
+          RTC in local TZ: no
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# timedatectl set-time '2021-12-18 12:31:00'
+Failed to set time: Automatic time synchronization is enabled
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# timedatectl set-ntp no
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# timedatectl set-time '2021-12-18 12:31:00'
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# date
+sáb 18 dic 2021 12:31:04 CST
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# timedatectl
+               Local time: sáb 2021-12-18 12:31:10 CST
+           Universal time: sáb 2021-12-18 18:31:10 UTC
+                 RTC time: sáb 2021-12-18 18:31:10
+                Time zone: America/Mexico_City (CST, -0600)
+System clock synchronized: no
+              NTP service: inactive
+          RTC in local TZ: no
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# 
+root@lm21:/home/cvh/dev/github/linux# exit
+exit
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ date
+sáb 18 dic 2021 12:31:24 CST
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ timedatectl
+               Local time: sáb 2021-12-18 12:31:32 CST
+           Universal time: sáb 2021-12-18 18:31:32 UTC
+                 RTC time: sáb 2021-12-18 18:31:27
+                Time zone: America/Mexico_City (CST, -0600)
+System clock synchronized: no
+              NTP service: inactive
+          RTC in local TZ: no
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+cvh@lm21:~/dev/github/linux$ 
+
